7f5c6cb1fa15bea70e0938554a4e994da6765811
larissaho <lho12@uw.edu>
2019-11-17 14:28:14 -0800

    created session

--- db/course_scrape.py
+++ db/course_scrape.py
@@ -2,15 +2,41 @@ import pyodbc
 import requests as r
 from bs4 import BeautifulSoup as bs
 
-import mechanize
-import urllib3
-import http.cookiejar as cookielib
+import urllib.request
+import sys
+
+from lxml import html
 
 # def getHTML(domain):
 #     frontPage = r.get(domain, auth=('USER_NAME', 'PASSWORD'))
 #     return bs(frontPage.content, 'html.parser')
 
 if __name__ == '__main__':
+
+    # Start the session 
+    session_requests = r.session()
+
+    # Account data 
+    login_data = {
+        'j_username' : '****', #FILL IN UWNET ID USERNAME 
+        'j_password' : '****!' #FILL IN UWNET ID PASSWORD
+    }
+
+    # Sign into the site with the account data
+    loginUrl = 'https://idp.u.washington.edu/idp/profile/SAML2/POST/SSO;jsessionid=7E0DB174EB9927C8F34EB6FBE6F5F826.idp07?execution=e1s1'
+    post = session_requests.post(
+        loginUrl,
+        data = login_data
+    )
+
+    # Access the site using the login data from the step before 
+    url = 'https://www.washington.edu/cec/'
+    result = session_requests.get(
+        url
+    )
+
+    print(result.content)
+
     # Call to scrape the given website domain
     # domain = 'https://www.washington.edu/cec/i/INFO200B1560.html'
     # scrapeContent = getHTML(domain)
@@ -19,16 +45,4 @@ if __name__ == '__main__':
 
     # print(content)
 
-    cook = cookielib.CookieJar()
-    req = mechanize.Browser()
-    req.set_cookiejar(cook)
-
-    req.open("https://www.washington.edu/cec/i/INFO200B1560.html")
-
-    req.select_form(nr=0)
-    req.form['username'] = 'username'
-    req.form['password'] = 'password.'
-    req.submit()
-
-    print(req.response().read())
     
