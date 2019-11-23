919732ece28a4523d2e506028f1805ae4db6f528
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-11-23 15:28:55 -0800

    UI components for displaying reviews

--- client/course-evals/src/App.js
+++ client/course-evals/src/App.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
-import {Container, TextField} from '@material-ui/core';
+import { Container, TextField } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import CourseResultCard from './course-result/CourseResult';
+import EvalsPage from './evals-page/EvalsPage';
+
+const Routes = {
+  home: "/",
+  results: "/:course"
+}
+
+const data = [
+  {
+    professorName: "Greg Hay",
+    reviews: ["a", "b", "c"]
+  },
+  {
+    professorName: "Amy Ko",
+    reviews: ["a", "b", "c"]
+  },
+  {
+    professorName: "David Ewald",
+    reviews: ["a", "b", "c"]
+  }
+]
 
 class App extends Component {
   constructor(props) {
@@ -20,28 +42,49 @@ class App extends Component {
   submitSearchInput = (evt) => {
     evt.preventDefault();
     if (evt.key === 'Enter') {
-      // TODO: Make an API Call
     }
   }
 
-  render = () => {
+  home = () => {
     return (
-      <Container maxWidth="xl">
-        <p>Course Evals</p>
+      <Container>
         <TextField
           value={this.state.searchTerm}
           id="standard-search"
-          label="Course Title"
+          label="Course Number"
           type="search"
           className={""}
           margin="normal"
           fullWidth={true}
-          onChange={evt => {this.updateSearchInput(evt)}}
-          onKeyUp={evt => {this.submitSearchInput(evt)}}
+          onChange={evt => { this.updateSearchInput(evt) }}
+          onKeyUp={evt => { this.submitSearchInput(evt) }}
         />
-        <CourseResultCard/>
-         
+        <br /><br /><br />
+        
+        <CourseResultCard />
       </Container>
+    );
+  }
+
+  evalsPage = () => {
+    return (
+      <EvalsPage courseTitle={"INFO 430"} ratings={data} />
+    );
+  }
+
+
+  render = () => {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path={Routes.home}>
+            {this.home()}
+          </Route>
+          <Route path={Routes.results}>
+            {this.evalsPage()}
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ client/course-evals/src/evals-page/EvalsPage.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { ListItem, ListItemText, Container, Card, CardHeader } from '@material-ui/core';
+import Eval from './../eval/Eval';
+
+class EvalsPage extends Component {
+    constructor(props) {
+        super(props);
+        let professorNames = this.props.ratings.map((rating) => { return rating.professorName });
+        let reviews = this.props.ratings.map((rating) => { return rating.reviews });
+        this.state = {
+            professors: professorNames,
+            reviews: reviews,
+            selectedIndex: 0
+        }
+    }
+
+    professorNames = (professors) => {
+        return professors.map((p, i) => {
+            return (
+                <ListItem key={`prof-${i}`}
+                    selected={this.state.selectedIndex === i}
+                    onClick={() => { this.selectProfessor(i) }} >
+                    <ListItemText primary={p} />
+                </ListItem>
+            )
+        });
+    }
+
+    reviews = (reviews) => {
+        return reviews.map((r, i) => {
+            return <Eval></Eval>
+        });
+    }
+
+    selectProfessor = (index) => {
+        this.setState({
+            selectedIndex: index
+        });
+    }
+
+
+    render() {
+        const styles = {
+            display: "flex",
+        };
+
+        return (
+            <Container>
+                <Card style={{ margin: "2% 0%", backgroundColor: "#303f9f", color: "#fff"}}>
+                    <CardHeader title={"INFO 430"}/>
+                </Card>
+
+                <div style={styles}>
+                    <div style={{width: "30%"}}>
+                        {this.professorNames(this.state.professors)}
+                    </div>
+                    <div>
+                        {this.reviews(this.state.reviews[this.state.selectedIndex])}
+                    </div>
+                </div>
+            </Container>
+        );
+    }
+}
+
+export default EvalsPage;
