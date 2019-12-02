0fcf777fac2401e7b6d442ed128840edc534b3e7
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-12-02 12:11:14 -0800

    Implemented APIs to get ratings data by courseID and professorID

--- server/api/read.js
+++ server/api/read.js
@@ -16,13 +16,79 @@ class ReadService {
     }
 
     async findRatingsByProfessorID(professorID) {
-        const result = [];
-        return result;
+        const request = new this.connection.Request();
+        request.input('P_ID', professorID);
+
+        let result = await request.execute('return_prof_reviews');
+        let rows = result.recordsets[0];
+        let byCourse = {};
+
+        rows.forEach((row, i) => {
+            let courseID = row.courseID;
+            let data = {};
+            let reviews = [];
+        
+            if (byCourse[courseID]) {
+                data = byCourse[courseID];
+                reviews = data.reviews;
+            } else {
+                data = {
+                    courseID: row.courseID,
+                    courseNumber: row.courseNumber,
+                    courseName: row.courseName,
+                    reviews: reviews
+                };
+            }
+
+            let r = {
+                rating: row.rating,
+                comment: row.comment
+            };
+            reviews.push(r);
+            data.reviews = reviews;
+            byCourse[courseID] = data;
+        });
+
+        let response = Object.values(byCourse);
+        return response;
     }
 
     async findRatingsByCourseID(courseID) {
-        const result = [];
-        return result;
+        const request = new this.connection.Request();
+        request.input('C_ID', courseID);
+
+        let result = await request.execute('return_course_reviews');
+        let rows = result.recordsets[0];
+        let byProf = {};
+
+        rows.forEach((row, i) => {
+            let profID = row.profID;
+            let data = {};
+            let reviews = [];
+
+            if (byProf[profID]) {
+                data = byProf[profID];
+                reviews = data.reviews;
+            } else {
+                data = {
+                    profID: row.profID,
+                    firstName: row.firstName,
+                    lastName: row.lastName,
+                    reviews: reviews
+                };
+            }
+
+            let r = {
+                rating: row.rating,
+                comment: row.comment
+            };
+            reviews.push(r);
+            data.reviews = reviews;
+            byProf[profID] = data;
+        });
+
+        let response = Object.values(byProf);
+        return response;
     }
 }
 
